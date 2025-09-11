7303e63d91ac46599428c6945fdfcda58fd941e1
elouiridev-dotcom <elouiridev@gmail.com>
2025-09-11 16:22:03 +0100

    save all changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = ["Full Stack Web Developer"];
+  const toRotate = ["Mern Stack Web Developer"];
   const period = 2000;
 
   useEffect(() => {
